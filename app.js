389e257a8c36d54e15c19a60c53a27cbaace9882
Suraj Kumar <sk658139@gmail.com>
2026-02-04 23:16:03 +0530

    - created database and connected backend and basic api structured. - Created basic HTML structure for the application with a welcome page and forms for creating and editing listings. - Styled the application with a simple CSS file. - Established a consistent layout using EJS templates for rendering views.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000;
 const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 
 
@@ -21,8 +23,10 @@ async function main() {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine("ejs", ejsMate);
 
 
 
@@ -36,6 +40,13 @@ app.get("/listings",async (req,res)=>{
     res.render("listings/index.ejs", {allListings});
 });
 
+
+
+// new route to show form to create a new listing
+app.get("/listing/new", (req,res)=>{
+    res.render("listings/new.ejs");
+})
+
 // show route to display a specific listing 
 app.get("/listings/:id", async (req,res)=>{
     const {id} = req.params;
@@ -44,8 +55,39 @@ app.get("/listings/:id", async (req,res)=>{
 });
 
 
+// create route to add a new listing to the database
+app.post("/listings", async (req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect(`/listings`);
+});
 
 
+// update : edit and update route 
+// two steps : 
+// 1. show edit form (get request : /listings/:id/edit) , 2. update the listing in the database (put request : /listings/:id )
+
+// edit route 
+app.get("/listings/:id/edit", async (req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+// update route 
+app.put("/listings/:id", async (req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}); 
+
+
+// delete route to delete a listing from the database
+app.delete("/listings/:id", async (req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect(`/listings`);
+});
 
 
 
